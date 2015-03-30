b52572b325ec06136158b78b96d9e5d6c138f2f9
subaru45 <cerasus45@gmail.com>
2015-03-30 16:46:03 +0900

    Adds README and source

---
+++ notif.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+DATE_FORMAT="%Y-%m-%d %H:%M:%S"
+SLEEP_TIME=15m
+
+
+usage_exit() {
+    echo "usage: $0 [-t SLEEPTIME] [FILENAME]" 1>&2
+    exit 1
+}
+
+get_charnum() {
+    echo `wc -m < $1`
+}
+
+generate_msg() {
+    echo "$SLEEP_TIMEで書いた文字数: $1文字"
+}
+
+
+while getopts t:h OPT
+do
+    case $OPT in
+        t) SLEEP_TIME=$OPTARG
+            ;;
+        h) usage_exit
+            ;;
+        \?) usage_exit
+            ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+if [ $# -ne 1 ]; then
+    usage_exit
+fi
+
+
+CHNUM_PREV=`get_charnum $1`
+
+while [ 1 ]
+do
+    sleep $SLEEP_TIME
+    if [ $? -ne 0 ]; then
+        usage_exit
+    fi
+
+    CHNUM=`get_charnum $1`
+    CHDIFF=`expr $CHNUM - $CHNUM_PREV`
+    notify-send \
+        "`generate_msg $CHDIFF`" \
+        "[`date +"$DATE_FORMAT"`]"
+    
+    CHARNUM=$TMPCHNUM
+done
