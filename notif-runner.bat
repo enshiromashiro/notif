1c764a255f8979d73c87287a2ca410b749a6188c
subaru45 <cerasus45@gmail.com>
2015-04-02 22:44:48 +0900

    Fixes a bug; occurring error when specify space including path to -filename

--- notif-runner.bat
+++ notif-runner.bat
@@ -1,9 +1,9 @@
 @echo off
-@rem notif.ps1を起動するだけのバッチ
 
+@rem notif.ps1を起動するだけのバッチ
 @rem PowerShellのスクリプトにはドラッグ&ドロップできないため
 
-powershell "%~dp0notif.ps1" "%~f1"
+powershell "& '%~dp0notif.ps1' '%~f1'"
 
 if not %ERRORLEVEL% == 0 (
   pause
